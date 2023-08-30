afeed78c747aa1aafa5bd3871e5074bf7da128ab
DoHunee <j981104@naver.com>
2023-08-30 18:02:12 +0900

    마무리

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React from "react";
 import './App.css';
-import { HashRouter, Route } from 'react-router-dom';
-import About from './routes/About';
+import {HashRouter,Route} from 'react-router-dom';
+import About from './routes/About'
 import Home from './routes/Home';
-import Navigation from './components/Navigation';
-import Detail from './routes/Detail';
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
 function App(){
-  return (
-  <HashRouter>
+  return(
+    <HashRouter>
+        {/* <Route path="/home">
+        <h1>Home</h1>
+        </Route>
+        <Route path="/home/introduction">
+          <h1>introduction</h1>
+        </Route>
+        <Route path ="/about">
+          <h1>About</h1>
+        </Route> */}
     <Navigation />
-    <Route path= "/" exact={true} component={Home} />
-    <Route path= "/about" component={About} />
-    <Route path= "/movie-detail" component={Detail} />
-    
+    <Route path="/" exact={true} component={Home}/>
+    <Route path="/about" component={About}/>
+    <Route path="/movie-detail" component={Detail} />
   </HashRouter>
+
+
   );
 }
+
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,41 +1,35 @@
-import PropTypes from "prop-types";
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Movie.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Movie.css';
+import { Link } from 'react-router-dom';
 
-function Movie({ title, year, summary, poster, genres }) {
-  // 파라미터로 받아옴
+function Movie({ title, year, summary, poster ,genres }) {
   return (
-    <div>
-      <img src={poster} alt={title} title={title} />
-      <div className="movie">
-        <Link
-          to={{
-            pathname: "/movie-detail",
-            state: { year, title, summary, poster, genres },
-          }}
+    <div className="movie">
+      <Link
+        to={{
+          pathname: '/movie-detail' ,
+          state: { year, title, summary, poster, genres},
+        }}
         >
-          <h3 className="movie_title">{title}</h3>
-          <h5 className="movie_year">{year}</h5>
-          <ul className="movie_genres">
-            {genres.map((genre, index) => {
-              return (
-                <li key={index} className="movie_genre">
-                  {genre}
-                </li>
-              );
-            })}
-          </ul>
-          <p className="movie_summary">{summary.slice(0, 200)}</p>
-        </Link>
-      </div>
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre,index) =>{
+              return <li key={index} className="movie__genre">{genre}</li>
+          })}
+        </ul>
+        <p className="movie__summary">{summary.slice(0,180)}...</p>
+       </div>
+      </Link>
     </div>
   );
 }
 
-Movie.prototype = {
-  // dto에서 정의한 것과 같은 효과
-  year: PropTypes.string.isRequired,
+Movie.propTypes = {
+  year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
